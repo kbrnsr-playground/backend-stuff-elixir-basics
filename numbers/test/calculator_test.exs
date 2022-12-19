2d68938bf5d166b6c12047862690613f3c09568e
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-19 22:20:15 +0000

    derpa derp

--- numbers/test/calculator_test.exs
+++ numbers/test/calculator_test.exs
@@ -6,5 +6,7 @@ defmodule CalculatorTest do
     assert Calculator.find_percentage_of_value(2.4, 100) === 2.4
   end
 
-  # test "find_distance_traveled "
+  test "find_distance_traveled (2.5, 55) = 137.5" do
+    assert Calculator.find_distance_traveled(2.537, 55) === 139.54
+  end
 end
