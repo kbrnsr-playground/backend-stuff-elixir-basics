fc51d36b61fd4b99c9768546d7c1a7845195bfbf
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-20 14:09:15 +0000

    depr

--- all_strings/lib/all_strings.ex
+++ all_strings/lib/all_strings.ex
@@ -2,17 +2,21 @@ defmodule AllStrings do
   @moduledoc """
   Documentation for `AllStrings`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> AllStrings.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def trim_string(value) do
+    String.trim(value)
   end
+
+  def first_letter(value) do
+    trim_string(value)
+    |> String.first()
+  end
+
+  def initial(value) do
+    first_letter(value)
+    |> String.capitalize()
+  end
+
+  #def initials(full_name) do
+  #  String.split(full_name)
+  #end
 end
