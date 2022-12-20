3a2310215ab64a003d1fc6319c54158fa0105c92
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-20 15:30:51 +0000

    final commit

--- all_strings/lib/all_strings.ex
+++ all_strings/lib/all_strings.ex
@@ -12,11 +12,46 @@ defmodule AllStrings do
   end
 
   def initial(value) do
-    first_letter(value)
-    |> String.capitalize()
+    "#{first_letter(value) |> String.capitalize()}. "
   end
 
-  # def initials(full_name) do
-  #  String.split(full_name)
-  # end
+  def initials(full_name) do
+    list = String.split(full_name)
+
+    Enum.map(list, fn name ->
+      initial(name)
+    end)
+    |> List.to_string()
+    |> trim_string()
+  end
+
+  def display_saucer do
+    IO.puts(build_saucer())
+  end
+
+  defp build_saucer do
+    """
+                        %%%%%%%%%%%%%%%%%%%%
+                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+          ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+      ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+    ************************************************************
+        ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+            ,,,&&&,,,,,,&&&,,,,,,&&&,,,,,,&&&,,,,,,&&&,,,
+                ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+    """
+  end
+
+  def display_customized_spacecraft(full_name) do
+    IO.puts(customized_spacecraft(full_name))
+  end
+
+  defp customized_spacecraft(full_name) do
+    build_saucer()
+    |> String.replace(
+      "************************************************************",
+      "******************** #{initials(full_name)}'s SPACECRAFT ********************"
+    )
+  end
 end
