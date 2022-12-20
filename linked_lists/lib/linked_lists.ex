45f7bf7ae13c3951451bac6bf3220433034099f4
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-20 17:05:56 +0000

    add functionality for equipment management

--- linked_lists/lib/linked_lists.ex
+++ linked_lists/lib/linked_lists.ex
@@ -2,17 +2,41 @@ defmodule LinkedLists do
   @moduledoc """
   Documentation for `LinkedLists`.
   """
+  def get_equipment_list() do
+    ["Space helmet", "Space suit", "Snacks", "Grappling hook", "Probe"]
+  end
 
-  @doc """
-  Hello world.
+  def get_first_item(list) do
+    hd(list)
+  end
 
-  ## Examples
+  def remove_first_item(list) do
+    tl(list)
+  end
 
-      iex> LinkedLists.hello()
-      :world
+  def add_slow(list, value) do
+    list ++ [value]
+  end
 
-  """
-  def hello do
-    :world
+  defp prepend_to_list(list, value) do
+    [value | list]
+  end
+
+  def add_fast(list, value) do
+    Enum.reverse(list)
+    |> prepend_to_list(value)
+    |> Enum.reverse()
+  end
+
+  def remove_item(list, index) do
+    List.delete_at(list, index)
+  end
+
+  def equipment_count(list) do
+    length(list)
+  end
+
+  def check_for_item(list, item) do
+    item in list
   end
 end
