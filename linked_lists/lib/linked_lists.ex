f9cccb01a8acdc0267a6a266f68cf44609639000
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-20 16:07:37 +0000

    add mix project linked_lists
    mix new linked_lists

---
+++ linked_lists/lib/linked_lists.ex
@@ -0,0 +1,18 @@
+defmodule LinkedLists do
+  @moduledoc """
+  Documentation for `LinkedLists`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LinkedLists.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
