274dad75ab6f62f16c1959021e4a21d2a5de34e8
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-20 21:22:11 +0000

    add recursion functions

--- recursion/lib/recursion.ex
+++ recursion/lib/recursion.ex
@@ -3,16 +3,30 @@ defmodule Recursion do
   Documentation for `Recursion`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Recursion.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def get_equipment_list() do
+    ["Space helmet", "Space suit", "Snacks", "Snacks", "Grappling hook", "Probe"]
   end
+
+  def loop([]), do: nil
+
+  def loop([head | tail]) do
+    IO.puts(head)
+    loop(tail)
+  end
+
+  def equipment_count([]), do: 0
+  def equipment_count([_head | tail]), do: 1 + equipment_count(tail)
+  def format_equipment_list([]), do: []
+
+  def format_equipment_list([head | tail]) do
+    [
+      head |> String.downcase() |> String.replace(" ", "_")
+      | format_equipment_list(tail)
+    ]
+  end
+
+  def occurence_count([], _value), do: 0
+  def occurence_count([_head | _tail], "Snacks"), do: 400
+  def occurence_count([value | tail], value), do: 1 + occurence_count(tail, value)
+  def occurence_count([_head | tail], value), do: occurence_count(tail, value)
 end
