890911eccd1db1af4bc850886abac02a3a657be2
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-20 23:24:16 +0100

    Recursion over lists in Elixir (#8)
    * add recursion mix project
    Executed "mix new recursion" in terminal
    * add recursion project to devcontainer workspace
    * add functionality to recursion module
    Added functionality to search a list of strings with a partial strings and return matched strings in a list.
    Added tests for this functionality.

---
+++ recursion/lib/recursion.ex
@@ -0,0 +1,42 @@
+defmodule Recursion do
+  @moduledoc """
+  Documentation for `Recursion`.
+  """
+
+  def get_equipment_list() do
+    ["Space helmet", "Space suit", "Snacks", "Snacks", "Grappling hook", "Probe"]
+  end
+
+  def loop([]), do: nil
+
+  def loop([head | tail]) do
+    IO.puts(head)
+    loop(tail)
+  end
+
+  def equipment_count([]), do: 0
+  def equipment_count([_head | tail]), do: 1 + equipment_count(tail)
+  def format_equipment_list([]), do: []
+
+  def format_equipment_list([head | tail]) do
+    [
+      head |> String.downcase() |> String.replace(" ", "_")
+      | format_equipment_list(tail)
+    ]
+  end
+
+  def occurence_count([], _value), do: 0
+  def occurence_count([_head | _tail], "Snacks"), do: 400
+  def occurence_count([value | tail], value), do: 1 + occurence_count(tail, value)
+  def occurence_count([_head | tail], value), do: occurence_count(tail, value)
+
+  def partial_occurence([], _occurence), do: []
+
+  def partial_occurence([head | tail], occurence) do
+    if String.contains?(head, occurence) do
+      [head | partial_occurence(tail, occurence)]
+    else
+      partial_occurence(tail, occurence)
+    end
+  end
+end
