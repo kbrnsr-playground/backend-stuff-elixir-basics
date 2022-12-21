9a2c62329eff3e84568691a37d7938e0f4ffba0b
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-21 11:53:14 +0000

    add functionality to pattern_matching
    Finished coding from the YouTube video, only need to complete the
    challenges

---
+++ pattern_matching/lib/saucer_preflight.ex
@@ -0,0 +1,12 @@
+defmodule SaucerPreflight do
+  defp max_flying_load_lbs, do: 55
+  defp convert_kg_to_lb(kg_value), do: kg_value * 2.2
+
+  # write a recursive loop that will convert kg to lbs
+  # elm[0]
+  # add the weights together to get total
+  def is_under_max_load?(list) do
+    # Call your recursive function
+    # return boolean
+  end
+end
