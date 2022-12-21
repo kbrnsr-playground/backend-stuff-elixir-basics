a150c2143c3c9010eefea5001fed96382ebede20
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-21 17:08:39 +0000

    fix failing test in apps/linked_lists/test

--- elixir_basics/apps/linked_lists/test/linked_lists_test.exs
+++ elixir_basics/apps/linked_lists/test/linked_lists_test.exs
@@ -2,7 +2,7 @@ defmodule LinkedListsTest do
   use ExUnit.Case
   doctest LinkedLists
 
-  test "greets the world" do
-    assert LinkedLists.hello() == :world
+  test "is space helmet in equipment list?" do
+    assert Enum.member?(LinkedLists.get_equipment_list(), "Space suit") == true
   end
 end
