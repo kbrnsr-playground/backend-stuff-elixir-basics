d89dca3119095bc811f57d27f62b25ee4ccd095f
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-21 22:55:46 +0000

    add doc and test for equipment_details

--- elixir_basics/apps/pattern_matching/lib/equipment_details.ex
+++ elixir_basics/apps/pattern_matching/lib/equipment_details.ex
@@ -1,8 +1,20 @@
 defmodule EquipmentDetails do
+  @moduledoc """
+  Provides list of each piece of equipment as well as weight and
+  quantity of each item
+  """
+
+  @doc """
+  Provides list of equipment specified as atoms
+  """
   def get_equipment_list() do
     [:space_helmet, :space_suit, :snacks, :grappling_hook, :probe]
   end
 
+  @doc """
+  Get tuple of equipment weight and quantity
+  raise runtime error if equipment piece doesn't exist
+  """
   def item_details(:space_helmet) do
     {3, :kg, 1}
   end
