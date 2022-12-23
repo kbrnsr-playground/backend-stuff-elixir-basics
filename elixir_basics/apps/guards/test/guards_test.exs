5e2207ac0991803244984d133bd7bc0be78b41a7
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-23 15:29:52 +0000

    update guards Updated docs, tests and functionality

--- elixir_basics/apps/guards/test/guards_test.exs
+++ elixir_basics/apps/guards/test/guards_test.exs
@@ -2,7 +2,31 @@ defmodule GuardsTest do
   use ExUnit.Case
   doctest Guards
 
-  test "greets the world" do
-    assert Guards.hello() == :world
+  describe "type" do
+    test "invalid type is passed" do
+      assert Guards.type("dangit!") == :not_number
+    end
+
+    test "nothing is passed" do
+      assert Guards.type() == :not_number
+    end
+  end
+
+  describe "is_single_digit" do
+    test "negative two digit integer is passed" do
+      assert Guards.is_single_digit(-10) == false
+    end
+
+    test "string is passed" do
+      assert Guards.is_single_digit("") == false
+    end
+
+    test "nothing is passed" do
+      assert Guards.is_single_digit() == false
+    end
+  end
+
+  test "return_even_numbers nothing is passed" do
+    assert Guards.return_even_numbers() == :not_even
   end
 end
