cd99715aba464ea49e2c2e76361048ab9d9f3c26
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-23 16:49:16 +0000

    update pattern_matching Update pattern_matching docs, tests and functionality to solve code challenge

--- elixir_basics/apps/pattern_matching/test/equipment_details_test.exs
+++ elixir_basics/apps/pattern_matching/test/equipment_details_test.exs
@@ -1,4 +1,10 @@
 defmodule EquipmentDetailsTest do
   use ExUnit.Case
   doctest EquipmentDetails
+
+  test "item_details non-existing atom is passed" do
+    assert_raise RuntimeError, "unknown item", fn ->
+      EquipmentDetails.item_details(:jet_pack)
+    end
+  end
 end
