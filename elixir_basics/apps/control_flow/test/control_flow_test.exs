475246ef8a2e638e399e3989f45338de6836ab8b
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-24 10:22:56 +0000

    tests for check_max_load

--- elixir_basics/apps/control_flow/test/control_flow_test.exs
+++ elixir_basics/apps/control_flow/test/control_flow_test.exs
@@ -1,4 +1,14 @@
 defmodule ControlFlowTest do
   use ExUnit.Case
   doctest ControlFlow
+
+  describe "check_max_load" do
+    test "load is zero" do
+      assert ControlFlow.check_max_load(0) == "Safe to fly!"
+    end
+
+    test "load is negative integer" do
+      assert_raise FunctionClauseError, fn -> ControlFlow.check_max_load(-1) end
+    end
+  end
 end
