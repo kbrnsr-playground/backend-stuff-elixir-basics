d6d08dca2b28483e7b89f77b8f991c672cd1be92
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-24 14:01:35 +0000

    add mix project bitstrings

---
+++ elixir_basics/apps/bitstrings/lib/bitstrings.ex
@@ -0,0 +1,18 @@
+defmodule Bitstrings do
+  @moduledoc """
+  Documentation for `Bitstrings`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bitstrings.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
