78d5cb655380f6e74b6bb220d7ff041e5517330f
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-24 10:36:42 +0000

    tests for check_fuel_level

--- elixir_basics/apps/control_flow/test/control_flow_test.exs
+++ elixir_basics/apps/control_flow/test/control_flow_test.exs
@@ -11,4 +11,18 @@ defmodule ControlFlowTest do
       assert_raise FunctionClauseError, fn -> ControlFlow.check_max_load(-1) end
     end
   end
+
+  describe "check_fuel_level" do
+    test "fuel percentage is 24" do
+      assert ControlFlow.check_fuel_level(24) == "empty tank"
+    end
+
+    test "fuel percentage is a negative integer" do
+      assert_raise FunctionClauseError, fn -> ControlFlow.check_fuel_level(-232) end
+    end
+
+    test "fuel percentage is over 100" do
+      assert_raise FunctionClauseError, fn -> ControlFlow.check_fuel_level(101) end
+    end
+  end
 end
