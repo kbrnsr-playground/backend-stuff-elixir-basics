0f8aaf7f087a437e067fa365220df6af91a60697
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-24 14:31:17 +0000

    update bitstrings.ex Removed hello function and accompanying test function

--- elixir_basics/apps/bitstrings/lib/bitstrings.ex
+++ elixir_basics/apps/bitstrings/lib/bitstrings.ex
@@ -2,17 +2,4 @@ defmodule Bitstrings do
   @moduledoc """
   Documentation for `Bitstrings`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Bitstrings.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- elixir_basics/apps/bitstrings/test/bitstrings_test.exs
+++ elixir_basics/apps/bitstrings/test/bitstrings_test.exs
@@ -1,8 +1,4 @@
 defmodule BitstringsTest do
   use ExUnit.Case
   doctest Bitstrings
-
-  test "greets the world" do
-    assert Bitstrings.hello() == :world
-  end
 end
