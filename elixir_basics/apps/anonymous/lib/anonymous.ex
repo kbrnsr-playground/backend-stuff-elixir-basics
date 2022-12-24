6c2d67d4eb681164d789faddf4e8b55f4ea0a23a
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-12-24 13:21:15 +0000

    update 'anonymous' Removed hello function and accompanying tests

--- elixir_basics/apps/anonymous/lib/anonymous.ex
+++ elixir_basics/apps/anonymous/lib/anonymous.ex
@@ -2,17 +2,4 @@ defmodule Anonymous do
   @moduledoc """
   Documentation for `Anonymous`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Anonymous.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- elixir_basics/apps/anonymous/test/anonymous_test.exs
+++ elixir_basics/apps/anonymous/test/anonymous_test.exs
@@ -1,8 +1,4 @@
 defmodule AnonymousTest do
   use ExUnit.Case
   doctest Anonymous
-
-  test "greets the world" do
-    assert Anonymous.hello() == :world
-  end
 end
